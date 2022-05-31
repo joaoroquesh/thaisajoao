c1db7288e113f997b30868d8f9bcde0c825a921c
joaoroquesh <joaoroquesh@gmail.com>
2022-05-31 14:45:12 -0300

    1

--- script/senhas.js
+++ script/senhas.js
@@ -1,3 +1,27 @@
+
+function mascaraData( campo, e )
+{
+	var kC = (document.all) ? event.keyCode : e.keyCode;
+	var data = campo.value;
+	
+	if( kC!=8 && kC!=46 )
+	{
+		if( data.length==2 )
+		{
+			campo.value = data += '/';
+		}
+		else if( data.length==5 )
+		{
+			campo.value = data += '/';
+		}
+		else
+			campo.value = data;
+	}
+}
+
+
+
+
 var estacao1 = document.getElementById("estacao1");
 var senha1 = document.getElementById("senha1");
 var input1 = document.getElementById("input1");
